5e6bd103135670aa543e9f804a531f2e08afc0bc
junsik <junsik@junsikui-MacBookPro.local>
2020-08-27 07:17:29 +0900

    first commit

---
+++ app/config/constants.js
@@ -0,0 +1,11 @@
+export const FIREBASE_API_KEY = 'AIzaSyBbLQAiNdSgE8tuChDo8MOXsm3ZZGSfrYA';
+export const FIREBASE_AUTH_DOMAIN = 'bongmu-news.firebaseapp.com';
+export const FIREBASE_DATABASE_URL = 'https://bongmu-news.firebaseio.com';
+export const FIREBASE_PROJECT_ID = 'bongmu-news';
+export const FIREBASE_STORAGE_BUCKET = 'bongmu-news.appspot.com';
+export const FIREBASE_MESSAGING_SENDER_ID = '642898320912';
+
+export const FACEBOOK_APP_ID = '<YOUR_FACEBOOK_APP_ID>';
+
+//    appId: "1:642898320912:web:f3e837342686af395a3495",
+//    measurementId: "G-SM4HP62GQ0"
